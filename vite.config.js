ba7038c0c29dd0adc20f022a74011cf03769e6ac
kpalencia <kpalenciae@miumg.edu.gt>
2026-04-24 02:45:23 -0600

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default {
-  base: "/"
-}
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",
+});
